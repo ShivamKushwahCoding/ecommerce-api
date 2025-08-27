8d4211bf16a1d436dfe279405ccdab4fbf914154
ShivamKushwahCoding <kushwahshivam1603@gmail.com>
2025-08-28 01:09:45 +0530

    feat(api): add health endpoint and configure env/timezone

---
+++ app/Http/Controllers/Api/HealthController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class HealthController extends Controller
+{
+    public function __invoke(): JsonResponse
+    {
+        return response()->json([
+            'status' => 'ok',
+            'app'    => config('app.name'),
+            'env'    => config('app.env'),
+            'time'   => now()->toIso8601String(),
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\HealthController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
+| All API routes are prefixed with /api by default.
+| We keep this file minimal for now; more routes will be added step-by-step.
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/health', HealthController::class);
